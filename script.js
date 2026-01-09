7ca0754243387c26c06d37ed8a098d7574c5fd86
BOUZIDI-HOOVEX <bouzaidinajib196901@gmail.com>
2026-01-09 18:17:45 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function orderWhatsApp(productName) {
-  const phoneNumber = "212600000000"; // ⚠️ Mets ton numéro WhatsApp ici
+  const phoneNumber = "212623524354"; // ⚠️ Mets ton numéro WhatsApp ici
   const message = `Bonjour, je souhaite commander le produit : ${productName}. Merci.`;
   const url = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
